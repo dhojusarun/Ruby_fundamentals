7f62240f0ae4d35c9951a5193e3403076792d9df
sarun dhoju <sarundhoju775@gmail.com>
2025-12-22 11:53:15 +0000

    first ruby program completed

--- hello.rb
+++ hello.rb
@@ -1,1 +1,6 @@
 puts "Hello world"
+puts "I'm learning Ruby right now"
+puts "This is easier than I thought"
+puts "What a wonderful day"
+puts "Hello world"
+puts "Today's an awesome day for coding!"
