002153a309c7e23aefb82b6be6ef44e208c52889
sarun dhoju <sarundhoju775@gmail.com>
2025-12-24 06:01:44 +0000

    hashes complete

--- hashes.rb
+++ hashes.rb
@@ -4,3 +4,33 @@ puts food[1]
 puts food[4]
 food[4] = "Sushi"
 puts food[4]
+people = {
+"Jake" => 30,
+"Adam" => 31,
+"Mike" => 25
+}
+
+puts people["Jake"]
+
+puts people["Mike"]
+
+people["Mike"] = 27
+
+puts people["Mike"]
+things = {
+1 => "This is one",
+10.0 => "This is a float",
+true => "Booleans work too?!"
+}
+
+puts things[1]
+
+puts things[10.0]
+
+puts things[true]
+people = {
+"Jake" => 30,
+"Adam" => 31,
+"Mike" => 25
+}
+people["Jake"]
